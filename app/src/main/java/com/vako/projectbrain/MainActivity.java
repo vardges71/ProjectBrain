93195a08167abe9770e220fbbd64fd332c3aa327
Vardges Gasparyan <vardges71@gmail.com>
2020-07-17 04:43:39 -0400

    added menu

--- app/src/main/java/com/vako/projectbrain/MainActivity.java
+++ app/src/main/java/com/vako/projectbrain/MainActivity.java
@@ -1,8 +1,14 @@
 package com.vako.projectbrain;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        ((MenuInflater) inflater).inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.userMenu:
+                // do something
+                return true;
+            case R.id.logout_icon:
+                logOut();
+                return true;
+            case R.id.search_icon:
+                searchUsers();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logOut() {
+
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+    }
+
+    private void searchUsers() {
+
+
     }
 }
